d0753ca67ff916095034bd16afb3e26df294e5ec
seher <pistachio@192.168.1.25>
2026-07-06 17:09:35 +0300

    Kodlara detaylı sunum açıklamaları eklendi ve son MFA testleri yapıldı.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,8 +3,14 @@ using SolarBackend.Models;
 
 namespace SolarBackend.Data;
 
+// DbContext sınıfından miras alıyoruz. Bu Microsoft'un veritabanı motorudur.
 public class AppDbContext : DbContext
 {
+    // Constructor (Yapıcı Metot): Dışarıdan gelen veritabanı ayarlarını (IP, Şifre) motora verir.
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // EĞER BİR YERE DbSet YAZARSAK: 
+    // Entity Framework arka planda gider ve PostgreSQL'de "Users" adında bir tablo oluşturur.
+    // Artık biz SQL INSERT/SELECT yazmak yerine, bu 'Users' listesine eleman ekleyip çıkaracağız.
     public DbSet<User> Users { get; set; }
 }
